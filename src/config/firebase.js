f030185df3991894fb646a08508dd5c9ec5b0dd0
rahull0328 <rahull0328@users.noreply.github.com>
2024-12-28 19:23:18 +0530

    Completed Signup & Login Functionality

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword ,getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCY5y-soFku8IvLbzZhs5MiKKgnIqS6JSI",
-  authDomain: "chatloom-d0103.firebaseapp.com",
-  projectId: "chatloom-d0103",
-  storageBucket: "chatloom-d0103.firebasestorage.app",
-  messagingSenderId: "588635725931",
-  appId: "1:588635725931:web:3bde9fb683c4ac776b512e",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
@@ -17,31 +21,44 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
 //whenever user creates an account this two document will be added by-default
 const signup = async (username, email, password) => {
-    try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        //user document
-        await setDoc(doc(db, "users", user.uid), {
-            id: user.uid,
-            username: username.toLowerCase(),
-            email,
-            name: "",
-            avatar: "",
-            bio: "Heyyy there, I'm Using ChatLoom !",
-            lastSeen: Date.now()
-        })
-        //chat-data
-        await setDoc(doc(db, "chats", user.uid), {
-            chatData: []
-        })
-        return user;
-    } catch (error) {
-        console.error(error);
-        toast.error(error.code);
-    }
-}
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    //user document
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name: "",
+      avatar: "",
+      bio: "Heyyy there, I'm Using ChatLoom !",
+      lastSeen: Date.now(),
+    });
+    //chat-data
+    await setDoc(doc(db, "chats", user.uid), {
+      chatData: [],
+    });
+    return user;
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code);
+  }
+};
 
-export {signup}
+//when user logs in this function will be called
+const Login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code);
+  }
+};
+
+export { signup, Login };
