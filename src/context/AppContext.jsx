3d946c6e2b9da48479023875da9982c0b62739e6
rahull0328 <rahull0328@users.noreply.github.com>
2024-12-30 19:17:24 +0530

    Completed Update Profile Module

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,13 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
-import { db } from "../config/firebase";
+import { auth, db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
+    const navigate = useNavigate();
     const [userData, setUserData] = useState(null);
     const [chatData, setChatData] = useState(null);
 
@@ -13,7 +15,23 @@ const AppContextProvider = (props) => {
         try {
             const userRef = doc(db, 'users', uid);
             const userSnap = await getDoc(userRef);
-            console.log(userSnap)
+            const userData = userSnap.data();
+            setUserData(userData);
+            if(userData.avatar && userData.name) {
+                navigate('/chat');
+            } else {
+                navigate('/profile');
+            }
+            await updateDoc(userRef, {
+                lastSeen: Date.now()
+            })
+            setInterval(async () => {
+                if (auth.chatUser) {
+                    await updateDoc(userRef, {
+                        lastSeen: Date.now()
+                    })
+                }
+            }, 60000);
         } catch (error) {
             
         }
