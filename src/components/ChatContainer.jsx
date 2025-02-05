9f12ebc954b1241880e0a34d4e85f77063c83cd8
rahull0328 <rmmehta2004@gmail.com>
2025-02-05 10:19:31 +0530

    home page completed

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
 
-export default ChatContainer
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages])
+  if (isMessagesLoading) return <div>Loading...</div>
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages...</p>
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
