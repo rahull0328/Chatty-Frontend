02b190111f29acc4858724c3cc5b4fcf71a318c9
rahull0328 <rmmehta2004@gmail.com>
2025-02-05 10:47:54 +0530

    added chat skeleton

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useChatStore } from "../store/useChatStore";
 import { useEffect } from "react";
 import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./Skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
@@ -9,7 +11,14 @@ const ChatContainer = () => {
   useEffect(() => {
     getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages])
-  if (isMessagesLoading) return <div>Loading...</div>
+
+  if (isMessagesLoading) return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessageInput />
+    </div>
+  )
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
